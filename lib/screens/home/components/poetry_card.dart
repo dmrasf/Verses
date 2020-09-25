a94d0bcb3a5723b023e2c0685be1d43935ec8241
dmrasf <dmrasf@qq.com>
2020-09-25 20:18:09 +0800

    Versec: poetry card

---
+++ lib/screens/home/components/poetry_card.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:Verses/contants.dart';
+
+import 'package:flutter_svg/flutter_svg.dart';
+
+class PoetryCard extends StatelessWidget {
+  const PoetryCard({Key key, @required this.poetry}) : super(key: key);
+
+  final Map<String, dynamic> poetry;
+
+  List<InlineSpan> getContent() {
+    List<InlineSpan> contents = List<InlineSpan>();
+    String content = poetry["内容"];
+    String pattern = "。：？；！";
+    List<String> lines = List<String>();
+    String line = "";
+
+    for (var i = 0, len = content.length; i < len; ++i) {
+      if (pattern.contains(content[i])) {
+        lines.add(line + content[i] + "\n");
+        line = "";
+      } else {
+        line = line + content[i];
+      }
+    }
+
+    for (var i = 0, len = lines.length; i < len; ++i) {
+      contents.add(TextSpan(
+        text: lines[i],
+        style: TextStyle(
+          color: kTextColor,
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+        ),
+      ));
+    }
+
+    return contents;
+  }
+
+  void press() {}
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.only(bottom: kDefaultPadding * 0.5),
+      padding: EdgeInsets.symmetric(
+        horizontal: kDefaultPadding,
+        vertical: kDefaultPadding * 0.6,
+      ),
+      width: size.width * 0.9,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              RichText(
+                text: TextSpan(
+                  text: "${poetry['作者']}\n${poetry['朝代']}",
+                  style: TextStyle(
+                    fontSize: 10,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              Spacer(),
+              Text(
+                "${poetry['题目']}",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Spacer(),
+              IconButton(
+                onPressed: press,
+                icon: SvgPicture.asset(
+                  "assets/icons/heart.svg",
+                  height: 20,
+                  width: 20,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.only(top: kDefaultPadding * 0.5),
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                children: getContent(),
+              ),
+            ),
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+        color: kPirmaryColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:Verses/contants.dart';
 import 'package:Verses/screens/home/components/title_and_search.dart';
+import 'package:Verses/screens/home/components/poetry_card.dart';
 import 'package:Verses/screens/home/components/today_and_more.dart';
+import 'dart:convert';
 
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -23,7 +25,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    String poetry =
+        '{ "题目": "凉州词二首 其一", "朝代": "唐", "作者": "王翰", "内容": "葡萄美酒夜光杯，欲饮琵琶马上催。醉卧沙场君莫笑，古来征战几人回。" }';
+
+    Map<String, dynamic> poe = json.decode(poetry);
     return Scaffold(
         appBar: buildAppBar(),
         body: SingleChildScrollView(
@@ -32,71 +37,7 @@ class HomeScreen extends StatelessWidget {
               children: [
                 TitleAndSearch(),
                 TodayPoetryAndMore(),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: kDefaultPadding,
-                    vertical: kDefaultPadding * 0.2,
-                  ),
-                  width: size.width * 0.9,
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                              text: "曹雪芹\n清",
-                              style: Theme.of(context).textTheme.button,
-                            ),
-                          ),
-                          Spacer(),
-                          Text(
-                            "好了歌注",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Spacer(),
-                          IconButton(
-                            onPressed: () {},
-                            icon: SvgPicture.asset(
-                              "assets/icons/heart.svg",
-                              height: 20,
-                              width: 20,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(top: kDefaultPadding * 0.5),
-                        child: RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(
-                            children: [
-                              TextSpan(text: "陋室空堂，当年笏满床；\n"),
-                              TextSpan(text: "衰草枯杨，曾为歌舞场。\n"),
-                              TextSpan(text: "蛛丝儿结满雕梁，绿纱今又糊在蓬窗上。\n"),
-                              TextSpan(text: "说什么脂正浓，粉正香，如何两鬓又成霜？\n"),
-                              TextSpan(text: "昨日黄土陇头送白骨，今宵红灯帐底卧鸳鸯。\n"),
-                              TextSpan(text: "金满箱，银满箱，展眼乞丐人皆谤。\n"),
-                              TextSpan(text: "正叹他人命不长，那知自己归来丧！\n"),
-                              TextSpan(text: "训有方，保不定日后作强梁。\n"),
-                              TextSpan(text: "择膏粱，谁承望流落在烟花巷！\n"),
-                              TextSpan(text: "因嫌纱帽小，致使锁枷杠。\n"),
-                              TextSpan(text: "昨怜破袄寒，今嫌紫蟒长：\n"),
-                              TextSpan(text: "乱烘烘你方唱罢我登场，反认他乡是故乡。\n"),
-                              TextSpan(text: "甚荒唐，到头来都是为他人作嫁衣裳！\n"),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  decoration: BoxDecoration(
-                    color: kPirmaryColor.withOpacity(0.2),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
+                PoetryCard(poetry: poe),
               ],
             ),
           ),
